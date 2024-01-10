f5316d6dbfba3d9236a34aec0a036810b71386f1
imagepointe-jk <148394472+imagepointe-jk@users.noreply.github.com>
2024-01-10 17:27:00 -0600

    Add functions and example

--- functions.ts
+++ functions.ts
@@ -1,11 +1,19 @@
 import fs from "fs";
-import xlsx from "xlsx";
+import xlsx, { WorkBook } from "xlsx";
+
+type Obj = {
+  [key: string]: any;
+};
+
+type DataFromWorkbook = {
+  [key: string]: Obj[];
+};
 
 function getSourceData(path: string) {
   try {
     const file = fs.readFileSync(path);
     const workbook = xlsx.read(file, { type: "buffer" });
-    const data: any = {};
+    const data: DataFromWorkbook = {};
     for (const sheetName of workbook.SheetNames) {
       data[`${sheetName}`] = xlsx.utils.sheet_to_json(
         workbook.Sheets[sheetName]
@@ -14,8 +22,44 @@ function getSourceData(path: string) {
 
     return data;
   } catch (error) {
-    console.error("ERROR PARSING SOURCE: ", error);
+    throw new Error("ERROR PARSING SOURCE");
   }
 }
 
-console.log(getSourceData("./sample1.xlsx"));
+function writeAsSheet(data: Obj[], filename: string) {
+  const sheet = xlsx.utils.json_to_sheet(data);
+  const workbook: WorkBook = {
+    Sheets: {
+      Sheet1: sheet,
+    },
+    SheetNames: ["Sheet1"],
+  };
+  xlsx.writeFile(workbook, `${filename}.xlsx`);
+}
+
+//like an array map, but instead of producing a new array, it produces a new spreadsheet using the given map function.
+export function spreadsheetMap(
+  inputPath: string,
+  inputSheetName: string,
+  outputName: string,
+  mapFn: (item: Obj, i: number, array: Obj[]) => any
+) {
+  try {
+    const sourceData = getSourceData(inputPath);
+    const mapped = sourceData[inputSheetName].map(mapFn);
+    writeAsSheet(mapped, outputName);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+//this code produced test.xlsx
+spreadsheetMap("sample1.xlsx", "Sheet1", "test", (item: Obj) => {
+  const newObj = {
+    id: item.id,
+    name: item.name,
+    startsWithLetter: item.name[0],
+    is35: item.age === 35,
+  };
+  return newObj;
+});
